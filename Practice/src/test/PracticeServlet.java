c3a4a29c69982698af2f25d79a379476ed149dc8
SriHarsha Akavaramu <sriharshaakavaramu@SriHarshas-MacBook-Pro.local>
2018-08-29 15:49:32 +0530

    hi this harsha

---
+++ Practice/src/test/PracticeServlet.java
@@ -0,0 +1,17 @@
+package test;
+import java.io.*;
+import javax.servlet.*;
+
+public class PracticeServlet extends GenericServlet{
+	public void init()
+	throws ServletException{
+		
+	}
+public void service(ServletRequest req,ServletResponse res)
+    throws ServletException,IOException{
+	PrintWriter pw = res.getWriter();
+	res.setContentType("text/html");
+	pw.println("Hii This Is SriHarsha Akavaramu ");
+}
+
+}
